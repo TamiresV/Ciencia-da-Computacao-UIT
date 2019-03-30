139affd33bf304d34ed2864a2ed5e54b14bdf58c
Tamires <40065379+TamiresV@users.noreply.github.com>
2019-03-29 21:04:38 -0300

    Add files via upload

---
+++ CompParalela/Produtor Consumidor/src/com/condelar/main/Main.java
@@ -0,0 +1,17 @@
+package com.condelar.main;
+
+import java.awt.Window;
+
+import com.condelar.obj.Controle;
+import com.condelar.view2.Windows;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		
+		Windows w = new Windows();
+		w.start();
+	}
+
+}
